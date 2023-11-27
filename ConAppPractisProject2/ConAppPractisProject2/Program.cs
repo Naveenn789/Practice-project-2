f04ebb1c5eb22f280526e2b34667e3ebc9340e46
Naveenn789 <naveengalla77@gmail.com>
2023-11-27 18:11:07 +0530

    Intial Commit

---
+++ ConAppPractisProject2/ConAppPractisProject2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPractisProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Mphasis\Practise Project\Rainbow School.txt";
+            FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+            string data;
+            using (StreamReader s = new StreamReader(file))
+            {
+                data = s.ReadToEnd();
+            }
+            Console.WriteLine(data);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
